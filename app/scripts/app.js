40bce70aa674ca9eeded4faad3fe5363f506a90c
mlamont8 <debiandmiat@hotmail.com>
2016-08-24 15:38:47 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name entsearchApp
+ * @description
+ * # entsearchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('entsearchApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name entsearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the entsearchApp
+ */
+angular.module('entsearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
