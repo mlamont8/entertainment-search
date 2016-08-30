ebaa05a8e8880baa47148358f323f85eb979c60e
mlamont8 <debiandmiat@hotmail.com>
2016-08-30 15:43:51 -0400

    update on movie search

--- app/scripts/controllers/movies.js
+++ app/scripts/controllers/movies.js
@@ -9,16 +9,14 @@
  */
  // http://api.themoviedb.org/3/movie/popular?api_key=21b0daca9dad79653c91d176b7930bee&text=emerald
 angular.module('entsearchApp')
-  .controller('MoviesCtrl', ['$scope', '$http','movieData',function ($scope, $http, movieData) {
+  .controller('MoviesCtrl', ['$scope', '$http', '$window','movieData',function ($scope, $http, $window, movieData) {
        // Function to send to movie id to moviedetail view when selection is clicked
     $scope.share = function(id){
-    
-    console.log(id);
-    movieData.set(id);
+        movieData.set(id);
  }; 
     $scope.results = [];
   // Returns popular movies on page load
-    return $http({
+     $http({
     		method: 'GET',
     		url: 'http://api.themoviedb.org/3/movie/popular?sort_by=popularity.desc',
             cache: true,
@@ -32,7 +30,25 @@ angular.module('entsearchApp')
     	}, function errorCallback(error){
     		console.error('error', error);
     	});
-   // };
+
+ $scope.search = function(){
+  $http({
+        method: 'GET',
+        url: 'http://api.themoviedb.org/3/movie/popular?sort_by=popularity.desc',
+            cache: true,
+        params: {
+          api_key: '21b0daca9dad79653c91d176b7930bee'
+          
+        }
+      }).then(function successCallback(data){
+        $scope.results = data;
+ //     
+      }, function errorCallback(error){
+        console.error('error', error);
+      });
+
+};
+
 
  
 
